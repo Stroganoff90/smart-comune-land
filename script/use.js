912b8d4f19c62cee6726469e24be07a535493f69
Stroganoff90 <39168951+Stroganoff90@users.noreply.github.com>
2020-02-10 09:16:54 +0400

    release

--- script/use.js
+++ script/use.js
@@ -45,4 +45,36 @@ $(document).ready(function(){
 			}
 		}
 	});
+
+	var $page = $('html, body');
+	$('a[href*="#"].navbar-item').click(function() {
+	    $page.animate({
+	        scrollTop: $($.attr(this, 'href')).offset().top
+	    }, 400);
+	    return false;
+	});
 });
+
+function hideOpen(){
+  document.getElementById("hide_menu").style.display = "block";
+};
+function hideClose(){
+  document.getElementById("hide_menu").style.display = "none";
+};
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" curr-sity", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " curr-sity";
+}
